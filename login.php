69025554a34cbc9d5ec31d6ff4bdf50cfee2bf3f
Lars Henrik Nordli <larshenrik91@gmail.com>
2017-05-05 10:54:04 +0200

    added simple login

--- login.php
+++ login.php
@@ -1,22 +1,29 @@
 <?php
+   session_start();
+   $_SESSION["login"] = false;
    include('.auth.php');
-   $feedback = 'Enter your username and password to access this site.';
+
+   $split = explode(':', $usrPassPair);
+   $correctUsername = $split[0];
+   $correctPassword = $split[1];
 
    if(isset($_POST['login_action'])){
-      $split = explode(':', $usrPassPair);
-      $username = split[0];
-      $password = split[1];
-
       $enteredUsername = $_POST['username'];
-      $enteredPassword = sha512($_POST['password']);
+      $enteredPassword = hash('sha512', $_POST['password']);
 
       if( ($enteredUsername == $correctUsername) && ($enteredPassword == $correctPassword) ){
+         $feedback = "Login successful!";
          $_SESSION['login'] = true;
+         header('Location: http://'. $_SERVER['HTTP_HOST']);
+         die();
       }
       else{
          $_SESSION['login'] = false;
-         $feedback;
+         $feedback = "Username or password is incorrect.";
       }
+   }
+   else{
+      $feedback = "Please enter username and password";
    }
 ?>
 <!DOCTYPE html>
@@ -24,20 +31,26 @@
    <head>
       <meta charset="utf-8">
       <title>LCD Virtual Assistant: Login</title>
-      <link type="stylesheet/css" href="style.css"/>
+      <link rel="stylesheet" type="text/css" href="style.css">
    </head>
    <body>
-      <h1>Login</h1>
-      <form class="login-form" name="login_action" action="index.html" method="post">
+      <h1>Please Log In</h1>
+
+      <p><?php if(isset($feedback)){ echo $feedback; }?></p>
+
+      <form class="login-form" action="" method="post">
+
          <div class="input-wrapper">
             <label for="username">Username:</label>
             <input type="text" name="username" id="username" value="" placeholder="jon"/>
          </div>
+
          <div class="input-wrapper">
             <label for="password">Password:</label>
             <input type="password" name="password" value=""/>
          </div>
-         <button type="submit">Log in<button>
+
+         <button type="submit" name="login_action">Log in</button>
       </form>
    </body>
 </html>
